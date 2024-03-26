958a9d20b7157a32318456483a7314fb73b9ef30
GabiruDLizard <kendricklknowles15@yahoo.com>
2024-03-26 16:59:53 -0300

    updated package.json name attribute

--- xmlWorker.js
+++ xmlWorker.js
@@ -2,14 +2,6 @@ const {XMLBuilder, XMLValidator} = require("fast-xml-parser");
 
 class XmlWorker{
 
-    // parseXML(info){
-    //     const builder = new XMLBuilder();
-    //     //const obj = JSON.parse(info);
-    //     const xmlContent = builder.build(info);
-    //     console.log(info);
-    //     return info;
-    // }
-
     validateXML(content){
         var result;
         try{
@@ -36,32 +28,4 @@ class XmlWorker{
     }
 }
 
-module.exports = XmlWorker;
-
-// const builder = new XMLBuilder();
-
-// function parseXML(info){
-//     const obj = JSON.parse(info);
-//     const xmlContent = builder.build(obj);
-//     return xmlContent;
-// }
-
-// function validateXML(content){
-//     const result = XMLValidator.validate(content, {
-//         allowBooleanAttributes: true
-//     });
-//     return result;
-// }
-
-// function XMLoperater(words){
-//     const xmlcontent = parseXML(words);
-//     if(validateXML(xmlcontent)){
-//         console.log(validateXML(xmlcontent));
-//     }
-//     else{
-//         console.log(validateXML(xmlcontent));
-//         return validateXML(xmlcontent);
-//     }
-// }
-
-// export {XMLoperater};
+module.exports = XmlWorker; 
